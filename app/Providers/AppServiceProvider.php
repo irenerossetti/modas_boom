07d8f0744408e4cccd55b611c03691ba32c999bb
Darwin Vigabriel <vigabriel.darwin@hotmail.com>
2025-11-28 04:23:38 -0400

    se añadio Dockerfile para el despliegue 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Registrar event listeners para auditoría
         $this->registerAuditoriaEvents();
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'login.throttle' => \App\Http\Middleware\LoginAttemptThrottle::class,
             'user.enabled' => \App\Http\Middleware\CheckUserEnabled::class,
